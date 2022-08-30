7588e11ff42378e250eb440da0b06ca4506f5fa4
goren <gorglebit@mail.ru>
2022-08-30 23:08:17 +0300

    Add project files.

---
+++ Source/ExtremeMining/ExtremeMining.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ExtremeMining.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ExtremeMining, "ExtremeMining" );
