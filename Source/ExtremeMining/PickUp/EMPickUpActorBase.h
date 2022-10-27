2396ccd142b28315cc88f2f2e26a172609eee395
Gleb Gorenkov <gorglebit@mail.ru>
2022-10-27 13:11:01 +0300

    Add pickup

---
+++ Source/ExtremeMining/PickUp/EMPickUpActorBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "EMPickUpActorBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXTREMEMINING_API AEMPickUpActorBase : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+};
